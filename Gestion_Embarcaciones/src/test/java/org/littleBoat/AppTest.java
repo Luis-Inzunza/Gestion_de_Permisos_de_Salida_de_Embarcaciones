7525956289270fad77718ddf832f9aa624a5e15e
josue <josuecanul2605@gmail.com>
2023-11-22 09:59:37 -0600

    Proyecto nuevo de Embarcaciones con los respectivos packages

---
+++ Gestion_Embarcaciones/src/test/java/org/littleBoat/AppTest.java
@@ -0,0 +1,18 @@
+package org.littleBoat;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testMain(){
+        System.out.println("Hello World");
+        assertEquals("Hello", "Hello");
+    }
+}
