de1fc410a786c4d1ffbecef2ad00f610ccb23f3f
Ileana-Castillo <ileanag.castillo@gmail.com>
2023-12-03 17:30:20 -0600

    feat: DAO Extintores con su documentacion y test

---
+++ Gestion_Embarcaciones/src/test/java/org/littleBoat/dao/ExtintoresDAOTest.java
@@ -0,0 +1,36 @@
+package org.littleboat.dao;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.littleboat.connection.H2Connection;
+import org.littleboat.dto.ExtintoresDTO;
+
+public class ExtintoresDAOTest {
+    ExtintoresDAO extintoresDAO = new ExtintoresDAO(H2Connection.getInstance());
+
+    @Test
+    void testInsert () {
+        ExtintoresDTO extintoresDTO = new ExtintoresDTO(765432123, LocalDate.now(), 3);
+        
+        Assertions.assertEquals(1, extintoresDAO.save(extintoresDTO));
+    }
+
+    @Test
+    void deleteTest () {
+        Assertions.assertEquals(1, extintoresDAO.deleteByMatricula(121234567));
+    }
+
+    @Test
+    void updateTest () {
+        ExtintoresDTO extintoresDTO = new ExtintoresDTO(121234567, LocalDate.now(), 3);
+        Assertions.assertEquals(1, extintoresDAO.update(extintoresDTO));
+    }
+    @Test
+    void findByMatricula () {
+        List <ExtintoresDTO> extintores = extintoresDAO.findByMatricula(900011122);
+        Assertions.assertEquals(1, extintores.size());
+    }
+}
