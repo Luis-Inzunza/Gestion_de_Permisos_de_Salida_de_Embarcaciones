d67626a254fb057116cae8ee86621ffb04328fe3
Samuel Rodriguez <116043050+Samux550@users.noreply.github.com>
2023-12-07 04:39:17 -0600

    Revert "Custom buttons hotfix"

---
+++ Gestion_Barcaciones/src/main/java/antlr/gestion_barcaciones/Gestion_Barcaciones.java
@@ -0,0 +1,14 @@
+
+
+package antlr.gestion_barcaciones;
+
+/**
+ *
+ * @author usuario
+ */
+public class Gestion_Barcaciones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- Gestion_Embarcaciones/src/main/java/org/LittleBoat/connection/ConnectionManager.java
+++
@@ -1,9 +0,0 @@
-package org.LittleBoat.connection;
-
-import java.sql.Connection;
-
-public interface ConnectionManager {
-    public Connection getConnection();
-
-    public void closeConnection(Connection connection);
-}
