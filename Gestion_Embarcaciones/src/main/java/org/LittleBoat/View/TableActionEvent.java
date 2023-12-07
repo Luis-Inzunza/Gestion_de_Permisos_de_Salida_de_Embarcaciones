e8099343a27f33ac3c8aebcf7f8000ee7321050b
Samuel Rodriguez <samux550@gmail.com>
2023-12-07 02:32:07 -0600

    se reinventaron los botones custom

--- Gestion_Embarcaciones/src/main/java/org/LittleBoat/View/TableActionEvent.java
+++
@@ -1,8 +0,0 @@
-package org.LittleBoat.View;
-
-public interface TableActionEvent {
-    public void onCertificadeButton(int row);
-    public void onCrewmateButton(int row);
-    public void onEditButton(int row);
-    public void onDeleteButton(int row);
-}
